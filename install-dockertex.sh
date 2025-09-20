9c4b7e311a693bfc1dd75a3f314ed594131d468a
BruceGliff <ilyaawaA@yandex.ru>
2025-09-20 16:50:28 +0300

    Adjusting scripts to work in local directory

--- install-dockertex.sh
+++ install-dockertex.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
 
 # 1. Установка Docker и Git
-echo "==> Installing Docker and Git packages"
-sudo add-apt-repository universe 
-sudo apt-get update
-sudo apt-get install -qy docker.io git
-sudo usermod -a -G docker $USER
+# skip
+# echo "==> Installing Docker and Git packages"
 
 # 2. Установка DockerTex
 echo "==> Downloading and updating docker image"
-sg docker -c "docker build -t raabf/texstudio-versions:texlive2018 ."
+sg docker -c "docker build -t raabf/texstudio-versions:latest ."
 
 # 3. Настройка DockerTex
 ## 3.1 Клонирование репозитория
 echo "==> Downloading dockertex"
-DOWNLOADS_DIR=$(xdg-user-dir DOWNLOAD)
-cd $DOWNLOADS_DIR && git clone https://github.com/raabf/dockertex.git
+DOWNLOADS_DIR=$(pwd)/external
+if [ ! -d $DOWNLOADS_DIR/dockertex ]; then
+  mkdir -p $DOWNLOADS_DIR
+  cd $DOWNLOADS_DIR && git clone https://github.com/raabf/dockertex.git && cd -
+fi
 
+
+DST_DIR=$(pwd)/scripts
+mkdir -p $DST_DIR
+mkdir -p $DST_DIR/desktop
+mkdir -p $DST_DIR/bin
 echo "==> Registering dockertex commands and shortcuts"
-## 3.2 Регистрация в системе - добавление команд dockertex и dockertexstudio
-sudo $DOWNLOADS_DIR/dockertex/install.sh --system --menu-volume DockerTexLive2018 --menu-tag texlive2018
+## 3.2 добавление команд dockertex и dockertexstudio в папку scripts/bin
+$DOWNLOADS_DIR/dockertex/install.sh --app-prefix $DST_DIR/desktop --bin-prefix $DST_DIR/bin --menu-volume DockerTexLive --menu-tag latest
 
 echo "==> Fixing start of TexStudio"
 ## 3.3 Исправление для запуска TexStudio
-sudo sed -i '/--net=host\ \\/d' /usr/local/bin/dockertexstudio
-
-## 3.4 Добавление переменной окружения с указанием версии DockerTex
-echo "==> Adding DockerTex tag to environment variables"
-echo "export DOCKERTEX_DEFAULT_TAG=texlive2018" >> ~/.profile
-echo "export DOCKERTEX_DEFAULT_TAG=texlive2018" >> ~/.bashrc
+sed -i '/--net=host\ \\/d' $DST_DIR/bin/dockertexstudio
+sed -i "s|HOME=/home/|HOME=$(pwd)|g; s|--workdir=/home/|--workdir=$(pwd)|g" $DST_DIR/bin/dockertexstudio
+sed -i '/image_tag="${DOCKERTEX_DEFAULT_TAG}"/i DOCKERTEX_DEFAULT_TAG=latest' $DST_DIR/bin/dockertexstudio
+sed -i '/image_tag="${DOCKERTEX_DEFAULT_TAG}"/i DOCKERTEX_DEFAULT_TAG=latest' $DST_DIR/bin/dockertex
+sed -i "s|HOME=/home/|HOME=$(pwd)|g" $DST_DIR/bin/dockertex
 
