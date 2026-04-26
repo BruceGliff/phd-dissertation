c0cdafb537af56343ae924a05fabb4078f2c77d8
BruceGliff <ilyaawaA@yandex.ru>
2026-04-26 17:17:14 +0300

    saturation

---
+++ src/sb-rfi.s
@@ -0,0 +1,11 @@
+Test SB+rfi-pos Allowed
+Histogram (4 states)
+11589 *>0:EAX=1; 0:EBX=0; 1:EAX=1; 1:EBX=0;
+3993715:>0:EAX=1; 0:EBX=1; 1:EAX=1; 1:EBX=0;
+3994308:>0:EAX=1; 0:EBX=0; 1:EAX=1; 1:EBX=1;
+388   :>0:EAX=1; 0:EBX=1; 1:EAX=1; 1:EBX=1;
+Ok
+
+Witnesses
+Positive: 11589, Negative: 7988411
+Condition exists (0:EAX=1 /\ 0:EBX=0 /\ 1:EAX=1 /\ 1:EBX=0) is validated
